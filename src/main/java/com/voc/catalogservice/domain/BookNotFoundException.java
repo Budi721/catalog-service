cf7d032b9c5054279e943eaae874e8b3c788dac5
budivalid <budi@val.id>
2024-08-28 00:04:20 +0700

    Init cloud native spring boot

---
+++ src/main/java/com/voc/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.voc.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+ public BookNotFoundException(String isbn) {
+ super("The book with ISBN " + isbn + " was not found.");
+ }
+}
